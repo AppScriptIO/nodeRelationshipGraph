c2edc63bdc2dd83046fde6cf77adc43e1a51fa2b
Dr. Safi <sfmissive@gmail.com>
2018-08-26 13:23:47 +0300

    ✒️ Package naming.

--- setup/script/hostMachine/containerManager.js
+++ setup/script/hostMachine/containerManager.js
@@ -1,11 +1,11 @@
 // run container manager with requested command.
 import path from 'path'
 import resolve from 'resolve'
-import { runManagerAppInContainerWithClientApp } from "@dependency/appDeploymentEnvironment"
+import { runManagerAppInContainerWithClientApp } from "@dependency/appDeploymentManager"
 const message_prefix = `\x1b[3m\x1b[2m•[${path.basename(__filename)} JS script]:\x1b[0m`
 console.group(`%s \x1b[33m%s\x1b[0m`,`${message_prefix}`,`ƒ container manager - container with volumes & requested entrypoint script`)
 
 runManagerAppInContainerWithClientApp({
     applicationHostPath: path.normalize(path.join(__dirname, '../../../')),
-    managerAppHostPath: path.dirname( resolve.sync('@dependency/appDeploymentEnvironment/package.json', { preserveSymlinks: true }) ) // use 'resolve' module to allow passing 'preserve symlinks' option that is not supported by require.resolve module.
+    managerAppHostPath: path.dirname( resolve.sync('@dependency/appDeploymentManager/package.json', { preserveSymlinks: true }) ) // use 'resolve' module to allow passing 'preserve symlinks' option that is not supported by require.resolve module.
 })
