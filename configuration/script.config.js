05ed899d517612d108359f7f03bb5e27a00e6e5f
Dr. Safi <sfmissive@gmail.com>
2019-02-04 04:27:19 +0200

    ➖ Script definitions which where moved to a common deploymentScript

--- configuration/script.config.js
+++ configuration/script.config.js
@@ -1,10 +1,7 @@
 const path = require('path')
 const resolvedModule = {
     get deploymentScript() { return path.dirname( require.resolve(`@dependency/deploymentScript/package.json`) )  },
-    get javascriptTestRunner() { return path.dirname( require.resolve(`@dependency/javascriptTestRunner/package.json`) ) }
 }
-
-console.log(`${resolvedModule.deploymentScript}/script`)
 
 module.exports = {
     script: [
@@ -15,17 +12,6 @@ module.exports = {
         {
             type: 'directory', 
             path: './script'
-        },
-        {
-            key: 'test',
-            type: 'module',
-            methodName: 'runTest',
-            adapterFunction: ({ callback, args }) => {
-                // change api to specific script parameter name
-                args[0].targetProject = args[0].api.project
-                return () => callback(...args) // specific interface of the callback
-            },
-            path: `${resolvedModule.javascriptTestRunner}/source/entrypoint/module/transpilation.entrypoint.js`,
         }
     ]
 }
