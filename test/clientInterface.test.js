4494b9e80a98de2d0a59e07a5dd83b8b0726d33e
Dr. Safi <sfmissive@gmail.com>
2019-03-01 04:57:39 +0200

    test update together with mocha instance changes.

--- test/clientInterface.test.js
+++ test/clientInterface.test.js
@@ -1,87 +1,98 @@
 process.env['SZN_DEBUG'] = true
 import assert from 'assert'
 import { assert as chaiAssertion } from 'chai'
-import { clientInterface, Plugin, InstanceContext, linkConstructor } from '../source/script.js'
+import { Graph, Plugin, InstanceContext, linkConstructor } from '../source/script.js'
 import { databaseModelAdapterFunction } from '../source/implementationPlugin/databaseModelAdapter/memoryModelAdapter.js'
 import * as graphData from './asset/graphData' // load sample data
 
-describe('Interface integration with the module in different initialization scenarios', () => {
-    describe.only('Initializing interface instance by function calling (Reflect.apply)', () => {
-        let configuredInterface = clientInterface({ pluginInstance: null, contextInstance: null, constructorPrototypeChain: null })
-        it('Should return configured interface', async () => {
-            console.log(configuredInterface)
-            assert(configuredInterface, '')
+suite('Interface integration with the module in different initialization scenarios', () => {
+    suite('Initializing interface instance by function calling (Reflect.apply)', () => {
+        let configuredGraph = Graph({ pluginInstance: null, contextInstance: null, constructorPrototypeChain: null })
+        test('Should return configured interface', async () => {
+            let traversalResult = await new configuredGraph({ nodeKey: 'node-key-1' })
+            console.log(traversalResult)
+            assert(traversalResult, '')
         })
+    })
+    suite('Initialize GraphController by instantiating interface using "new" keyword', () => {
+        test('Should return configured interface', async () => {
+            let traversalResult = await new Graph({ nodeKey: 'node-key-1' })
+            console.log(traversalResult)
+            assert(traversalResult, '')
+        })
+
+
+        // GraphControllerInstance.traverseGraph()
 
     })
-    describe('Initializing interface instance with pluggable instances of Plugin, Context, & constructorPrototypeChain', () => {
-        let plugin = new Plugin({
-                databaseModelAdapter: { // database simple memory adapter
-                    memoryModelAdapter: databaseModelAdapterFunction({ nodeArray: graphData.nodeDataItem.nodeArray })
-                }, 
-                graphTraversalImplementation: {
-                    aggregateIntoArray() {
-                        return require('./implementation/graphTraversalImplementation/debugImplementation.js').aggregateIntoArray
-                    },
-                    condition() {
-                        // return require('./implementation/graphTraversalImplementation/condition.js').condition
-                    },
-                    middleware() {
-                        // return require('./implementation/graphTraversalImplementation/middleware.js').middleware
-                    },
-                    schema() {
-                        // return require('./implementation/graphTraversalImplementation/schema.js').schema
-                    },
-                    shellscript() {
-                        // return require('./implementation/graphTraversalImplementation/shellscript.js').shellscript
-                    },
-                    template() {
-                        // return require('./implementation/graphTraversalImplementation/template.js').template
-                    },
-                }, 
-            },
-            {
-                defaultPlugin: {
-                    graphTraversalImplementation: 'aggregateIntoArray' 
-                }
-            }
-        )
+    // suite('Initializing interface instance with pluggable instances of Plugin, Context, & constructorPrototypeChain', () => {
+    //     let plugin = new Plugin({
+    //             databaseModelAdapter: { // database simple memory adapter
+    //                 memoryModelAdapter: databaseModelAdapterFunction({ nodeArray: graphData.nodeDataItem.nodeArray })
+    //             }, 
+    //             graphTraversalImplementation: {
+    //                 aggregateIntoArray() {
+    //                     return require('./implementation/graphTraversalImplementation/debugImplementation.js').aggregateIntoArray
+    //                 },
+    //                 condition() {
+    //                     // return require('./implementation/graphTraversalImplementation/condition.js').condition
+    //                 },
+    //                 middleware() {
+    //                     // return require('./implementation/graphTraversalImplementation/middleware.js').middleware
+    //                 },
+    //                 schema() {
+    //                     // return require('./implementation/graphTraversalImplementation/schema.js').schema
+    //                 },
+    //                 shellscript() {
+    //                     // return require('./implementation/graphTraversalImplementation/shellscript.js').shellscript
+    //                 },
+    //                 template() {
+    //                     // return require('./implementation/graphTraversalImplementation/template.js').template
+    //                 },
+    //             }, 
+    //         },
+    //         {
+    //             defaultPlugin: {
+    //                 graphTraversalImplementation: 'aggregateIntoArray' 
+    //             }
+    //         }
+    //     )
 
-        let sharedContext = new InstanceContext({
-            data: 'This string is shared between instances of the same context.'
-        })
+    //     let sharedContext = new InstanceContext({
+    //         data: 'This string is shared between instances of the same context.'
+    //     })
         
-        let constructorPrototypeChain = linkConstructor({
-            specificClassImplementation: {
-                GraphControllerMixin: null,
-                NodeFunction: null,
-                DataItemFunction: null        
-            }
-        })
+    //     let constructorPrototypeChain = linkConstructor({
+    //         specificClassImplementation: {
+    //             GraphControllerMixin: null,
+    //             NodeFunction: null,
+    //             DataItemFunction: null        
+    //         }
+    //     })
 
-        let configuredProxyInterface1 = clientInterface({
-            pluginInstance: plugin, 
-            contextInstance: sharedContext, 
-            constructorPrototypeChain
-        })
+    //     let configuredProxyInterface1 = clientInterface({
+    //         pluginInstance: plugin, 
+    //         contextInstance: sharedContext, 
+    //         constructorPrototypeChain
+    //     })
 
-        it('Should load plugins & adapter and traverse graph successfully', async () => {
-            const dataItemValueOfGraph = ['dataItem-key-1']
-            try {
-                // let nodeInstance = new Constructor2() // one way of creating instances
-                // or using a function in the constructor to create instnaces
-                // let resultArray = await Constructor1.traverseGraph({ nodeKey: 'node-key-1' })
-                // chaiAssertion.deepEqual(resultArray, dataItemValueOfGraph)
-            } catch (error) {
-                throw new Error(error)
-            }
-        })
+    //     it('Should load plugins & adapter and traverse graph successfully', async () => {
+    //         const dataItemValueOfGraph = ['dataItem-key-1']
+    //         try {
+    //             // let nodeInstance = new Constructor2() // one way of creating instances
+    //             // or using a function in the constructor to create instnaces
+    //             // let resultArray = await Constructor1.traverseGraph({ nodeKey: 'node-key-1' })
+    //             // chaiAssertion.deepEqual(resultArray, dataItemValueOfGraph)
+    //         } catch (error) {
+    //             throw new Error(error)
+    //         }
+    //     })
 
-    })
+    // })
 })
 
 
-// describe('database dapter registration to NodeRelationshipGraph', () => {
+// suite('database dapter registration to NodeRelationshipGraph', () => {
 //     const adapter = databaseModelAdapterFunction({ nodeArray: graphData.node }) // database simple memory adapter
 //     let nodeRelationshipGraph1 = new NodeRelationshipGraph({
 //         databaseModelAdapter: adapter
@@ -96,7 +107,7 @@ describe('Interface integration with the module in different initialization scen
 //         assert(nodeRelationshipGraph1.databaseModelAdapter == adapter)
 //     })
 // })
-// describe('Invoke NodeRelationshipGraph using static class, caching setting in Node process', () => {
+// suite('Invoke NodeRelationshipGraph using static class, caching setting in Node process', () => {
 //     NodeRelationshipGraph.registerAdapter({
 //         databaseModelAdapter: {
 //             getNodeDocumentQuery: async function() {}
@@ -107,7 +118,7 @@ describe('Interface integration with the module in different initialization scen
 //         assert.equal(typeof Controller, 'function')
 //     })
 // })
-// describe('Invoke NodeRelationshipGraph using instance class, allowing creation of different module settings', () => {
+// suite('Invoke NodeRelationshipGraph using instance class, allowing creation of different module settings', () => {
 //     let nodeRelationshipGraph = new NodeRelationshipGraph()
 //     nodeRelationshipGraph.registerAdapter({
 //         databaseModelAdapter: {
--- test/script.test.js
+++ test/script.test.js
@@ -1,14 +1,12 @@
 // process.env['SZN_DEBUG'] = true
 // import assert from 'assert'
 // import { assert as chaiAssertion } from 'chai'
-// import { clientInterface, Plugin, InstanceContext, linkConstructor } from './script.js'
-// import { databaseModelAdapterFunction } from './implementationPlugin/databaseModelAdapter/memoryModelAdapter.js'
-// import * as graphData from '../test/asset/graphData' // load sample data
+// import { Graph, Plugin, InstanceContext, linkConstructor } from '../source/script.js'
+// import { databaseModelAdapterFunction } from '../source/implementationPlugin/databaseModelAdapter/memoryModelAdapter.js'
+// import * as graphData from './asset/graphData' // load sample data
 
-// describe('Graph traversal scenarios - Traversing graphs with different implementations', () => {
-//     describe.only('Complete example of loading plugins and adapter (loading example plugins amd adapter) with graphData.nodeDataItem', () => {
-        
-
+// suite('Graph traversal scenarios - Traversing graphs with different implementations', () => {
+//     suite.only('Complete example of loading plugins and adapter (loading example plugins amd adapter) with graphData.nodeDataItem', () => {
 //         /*
 //         Previous implementation of client interface:
         
@@ -88,7 +86,7 @@
         
 //         let instance = new configuredProxyInterface1({ nodeKey: 'node-key-1' })
 
-//         it('Should load plugins & adapter and traverse graph successfully', async () => {
+//         test('Should load plugins & adapter and traverse graph successfully', async () => {
 //             const dataItemValueOfGraph = ['dataItem-key-1']
 //             try {
 //                 // let nodeInstance = new Constructor2() // one way of creating instances
@@ -100,7 +98,7 @@
 //             }
 //         })
 //     })
-//     describe('Testing graph traversal with execution of node dataItem (graphData.nodeDataItem).', () => {
+//     suite('Testing graph traversal with execution of node dataItem (graphData.nodeDataItem).', () => {
 //         let nodeRelationshipGraph = new NodeRelationshipGraph({
 //             databaseModelAdapter: databaseModelAdapterFunction({ nodeArray: graphData.nodeDataItem.nodeArray }) // database simple memory adapter
 //         })
@@ -112,7 +110,7 @@
 //             }
 //         })  
 //         let Controller = nodeRelationshipGraph.createStaticInstanceClasses()
-//         it('Should execute node dataItem according to each node`s executionType.', async () => {
+//         test('Should execute node dataItem according to each node`s executionType.', async () => {
 //             try {
 //                 let controller = await Controller.createContext({ 
 //                     traversalImplementationType: 'aggregateIntoArray' 
@@ -127,7 +125,7 @@
 //             }
 //         })        
 //     })
-//     describe('Testing graph traversal with `reference` dataItem type (graphData.nodeDataItemAsReference).', () => {
+//     suite('Testing graph traversal with `reference` dataItem type (graphData.nodeDataItemAsReference).', () => {
 //         let nodeRelationshipGraph = new NodeRelationshipGraph({
 //             databaseModelAdapter: databaseModelAdapterFunction({
 //                 nodeArray: graphData.nodeDataItemAsReference.nodeArray, 
@@ -142,7 +140,7 @@
 //             }
 //         })  
 //         let Controller = nodeRelationshipGraph.createStaticInstanceClasses()
-//         it('Should instantiate dataItem from reference key and execute dataItem according to each node`s executionType.', async () => {
+//         test('Should instantiate dataItem from reference key and execute dataItem according to each node`s executionType.', async () => {
 //             try {
 //                 let controller = await Controller.createContext({ 
 //                     traversalImplementationType: 'aggregateIntoArray' 
@@ -157,8 +155,8 @@
 //             }
 //         })        
 //     })
-//     describe('Testing graph traversal of with execution of node dataItem in parallel (graphData.nodeConnectionParallelTraversal).', () => {
-//         describe('Using `allPromise` traversal.', () => {
+//     suite('Testing graph traversal of with execution of node dataItem in parallel (graphData.nodeConnectionParallelTraversal).', () => {
+//         suite('Using `allPromise` traversal.', () => {
 //             let nodeRelationshipGraph = new NodeRelationshipGraph({
 //                 databaseModelAdapter: databaseModelAdapterFunction({ nodeArray: graphData.nodeConnectionParallelTraversal_allPromise.nodeArray }) // database simple memory adapter
 //             })
@@ -170,7 +168,7 @@
 //                 }
 //             })  
 //             let Controller = nodeRelationshipGraph.createStaticInstanceClasses()
-//             it('Should execute nodes in parallel fashion returning all resolved promises preserving order.', async () => {
+//             test('Should execute nodes in parallel fashion returning all resolved promises preserving order.', async () => {
 //                 const dataItemValueOfGraph = ['dataItem-key-0','dataItem-key-1','dataItem-key-2','dataItem-key-3','dataItem-key-4']
 //                 try {
 //                     let controller = await Controller.createContext({
@@ -184,7 +182,7 @@
 //                 }
 //             })        
 //         })
-//         describe('Using `raceFirstPromise` traversal.', () => {
+//         suite('Using `raceFirstPromise` traversal.', () => {
 //             let nodeRelationshipGraph = new NodeRelationshipGraph({
 //                 databaseModelAdapter: databaseModelAdapterFunction({ nodeArray: graphData.nodeConnectionParallelTraversal_raceFirstPromise.nodeArray }) // database simple memory adapter
 //             })
@@ -196,7 +194,7 @@
 //                 }
 //             })  
 //             let Controller = nodeRelationshipGraph.createStaticInstanceClasses()
-//             it('Should execute nodes in parallel fashion returning the first to resolve.', async () => {
+//             test('Should execute nodes in parallel fashion returning the first to resolve.', async () => {
 //                 const dataItemValueOfGraph = ['dataItem-key-0','dataItem-key-3']
 //                 try {
 //                     let controller = await Controller.createContext({
@@ -211,7 +209,7 @@
 //             })        
 //         })
 //     })
-//     describe('Testing graph traversal of outgoing node connections (graphData.nodeConnection).', () => {
+//     suite('Testing graph traversal of outgoing node connections (graphData.nodeConnection).', () => {
 //         let nodeRelationshipGraph = new NodeRelationshipGraph({
 //             databaseModelAdapter: databaseModelAdapterFunction({ nodeArray: graphData.nodeConnection.nodeArray }) // database simple memory adapter
 //         })
@@ -223,7 +221,7 @@
 //             }
 //         })  
 //         let Controller = nodeRelationshipGraph.createStaticInstanceClasses()
-//         it('Should load plugins & adapter successfully', async () => {
+//         test('Should load plugins & adapter successfully', async () => {
 //             const dataItemValueOfGraph = ['dataItem-key-1', 'dataItem-key-2']
 //             try {
 //                 let controller = await Controller.createContext({ 
@@ -237,7 +235,7 @@
 //             }
 //         })        
 //     })
-//     describe('Testing graph traversal of outgoing node connections that are grouped into ports (graphData.nodePort).', () => {
+//     suite('Testing graph traversal of outgoing node connections that are grouped into ports (graphData.nodePort).', () => {
 //         let nodeRelationshipGraph = new NodeRelationshipGraph({
 //             databaseModelAdapter: databaseModelAdapterFunction({ nodeArray: graphData.nodePort.nodeArray }) // database simple memory adapter
 //         })
@@ -249,7 +247,7 @@
 //             }
 //         })
 //         let Controller = nodeRelationshipGraph.createStaticInstanceClasses()
-//         it('Should load plugins & adapter successfully', async () => {
+//         test('Should load plugins & adapter successfully', async () => {
 //             const dataItemValueOfGraph = ['dataItem-key-0', 'dataItem-key-2', 'dataItem-key-4', 'dataItem-key-5', 'dataItem-key-1', 'dataItem-key-3']
 //             try {
 //                 let controller = await Controller.createContext({
@@ -266,9 +264,9 @@
 // })
 
 
-// describe('ReusableNestedUnit module - Connecting class inhiritance dynamically', () => {
+// suite('ReusableNestedUnit module - Connecting class inhiritance dynamically', () => {
 
-//     describe('Creating static classes without cache', () => {
+//     suite('Creating static classes without cache', () => {
         
 //         let Controller1 = createStaticInstanceClasses({ 
 //             Superclass: null, 
@@ -281,13 +279,13 @@
 //             cacheName: false
 //         })
 
-//         it('Should return different class references', () => {
+//         test('Should return different class references', () => {
 //             assert.notStrictEqual(Controller1, Controller2)
 //         })
     
 //     })
         
-//     describe('Caching static classes using different passed cacheName', () => {
+//     suite('Caching static classes using different passed cacheName', () => {
         
 //         let Controller1 = createStaticInstanceClasses({
 //             Superclass: null, 
@@ -300,13 +298,13 @@
 //             cacheName: 'Y'
 //         })
 
-//         it('Should return different class references', () => {
+//         test('Should return different class references', () => {
 //             assert.notStrictEqual(Controller1, Controller2)
 //         })
     
 //     })
 
-//     describe('Caching static classes using same passed cacheName', () => {
+//     suite('Caching static classes using same passed cacheName', () => {
         
 //         let Controller1 = createStaticInstanceClasses({ 
 //             Superclass: null, 
@@ -319,30 +317,30 @@
 //             cacheName: 'X'
 //         })
 
-//         it('Should return same class references', () => {
+//         test('Should return same class references', () => {
 //             assert.strictEqual(Controller1, Controller2)
 //         })
 
-//         it('Should return same subclass references', () => {
+//         test('Should return same subclass references', () => {
 //             assert.strictEqual(Controller1.extendedSubclass.static['NestedUnit'], Controller1.extendedSubclass.static['NestedUnit'])
 //             assert.strictEqual(Controller1.extendedSubclass.static['Unit'], Controller1.extendedSubclass.static['Unit'])
 //         })
 
 //     })
 
-//     describe('Check new instance chain when using mixinController', () => {
+//     suite('Check new instance chain when using mixinController', () => {
 //         const mixinController = (require('./implementation/condition').default).ControllerMixin
 //         let ClassChain = mixinController({ Superclass: class S {} }) /* return Specific implementation Controller */
 //         class Subclass extends ClassChain {}
 //         let i = new Subclass()
 
-//         it('Should not alter child class prototype, should create an object with prototype chain matching it\'s subclass class', () => {
+//         test('Should not alter child class prototype, should create an object with prototype chain matching it\'s subclass class', () => {
 //             assert.strictEqual(i.constructor.name, Subclass.name)
 //         })
 
 //     })
 
-// //     // describe.skip('Check instance prototype chain', () => {
+// //     // suite.skip('Check instance prototype chain', () => {
         
 // //     //     let Controller = createStaticInstanceClasses({
 // //     //         Superclass: Application, 
@@ -376,9 +374,9 @@
 // //     //     }
 // //     //     let instance = new Controller.extendedSubclass.static['NestedUnit']('43d6e114-54b4-47d8-aa68-a2ae97b961d5', portAppInstance)
 // //     //     let controller = new Controller(false, { portAppInstance })
-// //     //     let middlewareArray = controller.initializeNestedUnit({ nestedUnitKey: '43d6e114-54b4-47d8-aa68-a2ae97b961d5' })
+// //     //     let middlewareArray = controller.initializeNestedUntest({ nestedUnitKey: '43d6e114-54b4-47d8-aa68-a2ae97b961d5' })
         
-// //     //     it(' ', () => {
+// //     //     test(' ', () => {
 // //     //         assert.strictEqual(instance.initializeNestedUnit, controller.initializeNestedUnit)
 // //     //         // middlewareArray.then(x => {console.log(x)}).then(done)
 // //     //     })
