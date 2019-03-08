1865171e44374d26c3f3864ad7b441431e4a2778
Dr. Safi <sfmissive@gmail.com>
2019-03-08 23:19:37 +0200

    ➕👕 Adding linter config & packages - ESLint, Babel-eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parser: "babel-eslint",
+    "parserOptions": {
+        // babelOptions: {
+        //     configFile: "path/to/config.js",
+        // },      
+        // "ecmaVersion": 2018,
+        // "sourceType": "module"
+    },
+    "rules": {
+    },
+    env: {
+    }
+};
