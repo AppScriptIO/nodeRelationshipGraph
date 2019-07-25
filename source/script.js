547556a59ece60fd5d950d3032522e5fd86f0eb9
Safi <sfmissive@gmail.com>
2019-07-25 20:23:57 +0300

    ✒ Update export entrypoint keys.

--- source/script.js
+++ source/script.js
@@ -1,11 +1,11 @@
-export * from '@dependency/entity' // Export Symbol keys to allow consumer to use it's functionality.
+// export * from '@dependency/entity' // Export Symbol keys to allow consumer to use it's functionality.
 
-export * from './constructable/Cache.class.js'
-export * from './constructable/Connection.class.js'
-export * from './constructable/Context.class.js'
-export * from './constructable/Database.class.js'
-export * from './constructable/Evaluator.class.js'
-export * from './constructable/Graog.class.js'
-export * from './constructable/GraphElement.class.js'
-export * from './constructable/GraphTraversal.class.js'
-export * from './constructable/Node.class.js'
+export * as Cache from './constructable/Cache.class.js'
+export * as Connection from './constructable/Connection.class.js'
+export * as Context from './constructable/Context.class.js'
+export * as Database from './constructable/Database.class.js'
+export * as Evaluator from './constructable/Evaluator.class.js'
+export * as Graph from './constructable/Graph.class.js'
+export * as GraphElement from './constructable/GraphElement.class.js'
+export * as GraphTraversal from './constructable/GraphTraversal.class.js'
+export * as Node from './constructable/Node.class.js'
