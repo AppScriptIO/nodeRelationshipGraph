960037c794d4af4d26bae57572d22f38e6c739d4
Safi <sfmissive@gmail.com>
2019-07-25 19:59:10 +0300

    ✒ Move script files to deployment script library. 📦 Bump version package.json

--- script/memgraph.container.sh
+++
@@ -1,3 +0,0 @@
-docker run -p 7687:7687  memgraph
-
-#-v mg_lib:/var/lib/memgraph -v mg_log:/var/log/memgraph -v mg_etc:/etc/memgraph
--- source/implementationPlugin/graphTraversalImplementation/concreteFunction/processData.js
+++ source/implementationPlugin/graphTraversalImplementation/concreteFunction/processData.js
@@ -106,77 +106,74 @@ export async function executeShellscriptFile({ node, resourceNode }) {
  
 */
 
-let template = () => {
-  let _ = require('underscore')
-  /**
-   *
-   * @return {String} String of rendered HTML document content.
-   */
-  async function initializeNestedUnit({ nestedUnitKey, additionalChildNestedUnit = [], pathPointerKey = null }) {
-    // Entrypoint Instance
-    // views argument that will be initiallized inside templates:
-    // loop through template and create rendered view content.
-    let view = await nestedUnitInstance.loopInsertionPoint({ type: 'aggregateIntoTemplateObject' })
+// let underscore = require('underscore')
+/**
+ *
+ * @return {String} String of rendered HTML document content.
+ */
+async function initializeNestedUnit({ nestedUnitKey, additionalChildNestedUnit = [], pathPointerKey = null }) {
+  // views argument that will be initiallized inside templates:
+  // loop through template and create rendered view content.
+  let view = await nestedUnitInstance.loopInsertionPoint({ type: 'aggregateIntoTemplateObject' })
 
-    assert(this.portAppInstance.config.clientSidePath, "• clientSidePath cannot be undefined. i.e. previous middlewares should've set it")
-    let templatePath = path.join(this.portAppInstance.config.clientSidePath, unitInstance.file.filePath)
-    let renderedContent
-    switch (unitInstance.processDataImplementation) {
-      default:
-      case 'underscoreRendering':
-        renderedContent = await this.underscoreRendering({ templatePath, view })
-        break
-    }
-
-    switch (unitInstance.processRenderedContent) {
-      case 'wrapJsTag':
-        renderedContent = `<script type="module" async>${renderedContent}</script>`
-        break
-      default: // skip
-    }
-
-    return renderedContent
+  assert(this.portAppInstance.config.clientSidePath, "• clientSidePath cannot be undefined. i.e. previous middlewares should've set it")
+  let templatePath = path.join(this.portAppInstance.config.clientSidePath, unitInstance.file.filePath)
+  let renderedContent
+  switch (unitInstance.processDataImplementation) {
+    default:
+    case 'underscoreRendering':
+      renderedContent = await this.underscoreRendering({ templatePath, view })
+      break
   }
 
-  async function underscoreRendering({ templatePath, view }) {
-    // Load template file.
-    let templateString = await filesystem.readFileSync(templatePath, 'utf-8')
-    // Shared arguments between all templates being rendered
-    const templateArgument = {
-      templateController: this,
-      context: this.portAppInstance.context,
-      Application,
-      argument: {},
-    }
-    let renderedContent = _.template(templateString)(
-      Object.assign(
-        {},
-        templateArgument, // use templateArgument in current template
-        { view, templateArgument }, // pass templateArgument to nested templates
-      ),
-    )
-    return renderedContent
+  switch (unitInstance.processRenderedContent) {
+    case 'wrapJsTag':
+      renderedContent = `<script type="module" async>${renderedContent}</script>`
+      break
+    default: // skip
   }
 
-  function renderedContentString(viewName, viewObject) {
-    // loop throught the strings array to combine them and print string code to the file.
-    if (viewObject[viewName] && Array.isArray(viewObject[viewName])) {
-      return viewObject[viewName].join('') // joins all array components into one string.
-    }
-  }
+  return renderedContent
+}
 
-  let traversePort = async function aggregateIntoTemplateObject() {
-    let view = {}
-    if (this.insertionPoint) {
-      for (let insertionPoint of this.insertionPoint) {
-        let children = await this.filterAndOrderChildren({ insertionPointKey: insertionPoint.key })
-        let subsequent = await this.initializeInsertionPoint({ insertionPoint, children })
-        if (!(insertionPoint.name in view)) view[insertionPoint.name] = []
-        Array.prototype.push.apply(view[insertionPoint.name], subsequent)
-      }
-    }
-    return view
+async function underscoreRendering({ templatePath, view }) {
+  // Load template file.
+  let templateString = await filesystem.readFileSync(templatePath, 'utf-8')
+  // Shared arguments between all templates being rendered
+  const templateArgument = {
+    templateController: this,
+    context: this.portAppInstance.context,
+    Application,
+    argument: {},
   }
+  let renderedContent = underscore.template(templateString)(
+    Object.assign(
+      {},
+      templateArgument, // use templateArgument in current template
+      { view, templateArgument }, // pass templateArgument to nested templates
+    ),
+  )
+  return renderedContent
+}
+
+function renderedContentString(viewName, viewObject) {
+  // loop throught the strings array to combine them and print string code to the file.
+  if (viewObject[viewName] && Array.isArray(viewObject[viewName])) {
+    return viewObject[viewName].join('') // joins all array components into one string.
+  }
+}
+
+let traversePort = async function aggregateIntoTemplateObject() {
+  let view = {}
+  if (this.insertionPoint) {
+    for (let insertionPoint of this.insertionPoint) {
+      let children = await this.filterAndOrderChildren({ insertionPointKey: insertionPoint.key })
+      let subsequent = await this.initializeInsertionPoint({ insertionPoint, children })
+      if (!(insertionPoint.name in view)) view[insertionPoint.name] = []
+      Array.prototype.push.apply(view[insertionPoint.name], subsequent)
+    }
+  }
+  return view
 }
 
 /*
