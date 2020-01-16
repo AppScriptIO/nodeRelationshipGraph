13a9974632af168d7b03c994b3525f3325cf82c7
Safi <nassarsafi@gmail.com>
2020-01-16 03:04:46 +0200

    📦 Bump package.json version.

--- source/script.js
+++ source/script.js
@@ -1,9 +1,9 @@
 // export * from '@dependency/entity' // Export Symbol keys to allow consumer to use it's functionality.
 
 export * as Context from './constructable/Context.class.js'
-export * as Database from './constructable/Database.class.js'
+export * as Database from './constructable/Database'
 export * as Graph from './constructable/Graph'
-export * as Traversal from './constructable/Traversal.class.js'
+export * as Traversal from './constructable/Traversal'
 export * as schemeReference from './dataModel/graphSchemeReference.js'
 
 export { Entity } from '@dependency/entity' // used for referencing the symbols that are used by graph traversal.
